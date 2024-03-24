b61f3ab08416af01c9f8d679c647cc17324c2e05
rdoriol <rdoriol@gmail.com>
2024-03-24 13:36:12 +0100

    topheader

--- src/app/01-header/components/main-top-header/main-top-header.component.ts
+++ src/app/01-header/components/main-top-header/main-top-header.component.ts
@@ -14,4 +14,21 @@ export class MainTopHeaderComponent {
     // of type any for avoid duplicate code from interface definition Curriculum Vitae
   public cvRobertoChild: any = { }
 
+  public lightMode: boolean = false;
+
+  public changeMode() {
+    let icon = document.getElementById("icon_mode");
+
+    if(this.lightMode == false) {
+      icon!.setAttribute("src", "../assets/icons/moon-blue-50.x50.png");
+      this.lightMode = true;
+      document.getElementsByTagName("body")[0].setAttribute("style", "background-color: #f5f5f5;");
+    }
+    else {
+      icon!.setAttribute("src", "../assets/icons/sun-blue-50.x50.png");
+      this.lightMode = false;
+      document.getElementsByTagName("body")[0].removeAttribute("style");
+    }
+  }
+
 }
