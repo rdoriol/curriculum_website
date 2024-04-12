5dcf6a1b7691e9b27d7c61898162c9cd8781d1f7
rdoriol <rdoriol@gmail.com>
2024-04-12 14:11:11 +0200

    technologies

--- src/app/02-resume-content/components/technologies/technologies.component.ts
+++ src/app/02-resume-content/components/technologies/technologies.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-technologies',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './technologies.component.html',
   styleUrl: './technologies.component.css'
 })
 export class TechnologiesComponent {
 
-}
+  constructor() {}
+
+  @Input()
+  public cvRobertoChild:any = {};
+
+} // End class
