c6d5e730b3905b31894e6aa28da09bbc277ae183
rdoriol <rdoriol@gmail.com>
2024-04-19 13:11:21 +0200

    projects-carousel

--- src/app/02-resume-content/components/projects/projects.component.ts
+++ src/app/02-resume-content/components/projects/projects.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
 
-}
+  constructor() {}
+
+  @Input()
+  public cvRobertoChild: any = {};
+
+} // End class
