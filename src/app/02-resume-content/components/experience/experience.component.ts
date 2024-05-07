297ce79c15a1803d4812e067cab6c7d49c18f36d
rdoriol <rdoriol@gmail.com>
2024-05-07 18:46:35 +0200

    experience finished

--- src/app/02-resume-content/components/experience/experience.component.ts
+++ src/app/02-resume-content/components/experience/experience.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
 export class ExperienceComponent {
 
-}
+  constructor() {}
+
+  @Input()
+  public cvRobertoChild:any = {};
+
+  public scrollDownload(id:string) {
+    let selector = document.getElementById(id);
+    //.scrollTo(0, );
+     selector!.scrollIntoView({block: "end", behavior: "smooth"});
+  }
+
+}   // End class
